0ec77a901f5d4589bf3e4781443d281ceba406d8
Austin Merrick <austinmerrick@onsclom.net>
2024-10-22 19:30:23 -0700

    Back to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,4 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
-  server: {
-    headers: {
-      "ngrok-skip-browser-warning": "true",
-    }
-  }
-});
+})
