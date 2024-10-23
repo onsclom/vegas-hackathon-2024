328c1f593295e2aae28a4b0d22695d3a75f757d2
Austin Merrick <austinmerrick@onsclom.net>
2024-10-22 19:30:12 -0700

    Back to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: node({ mode: "standalone" }),
+  server: {
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+    }
+  }
 });
